0ace69b5412eb53a9229e9ec16da3c7294b603ea
Ali Khundmiri <salikhundmiri@gmail.com>
2020-09-25 11:28:37 +0530

    👤 🐾 created header and footer file

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.write(`
+	<footer class="mastfoot mt-auto">
+	<div class="inner">
+	<p>Have a doubt? Reach out to me via <a href="/instagram">Instagram</a> or <a href="/twitter">Twitter</a>.</p>
+	</div>
+	</footer>
+	`);
---
+++ header.js
@@ -0,0 +1,13 @@
+// Update this file for common header
+
+document.write(`
+		<header class="masthead mb-auto">
+			<div class="inner">
+				<h3 class="masthead-brand">Ali Coder Maker</h3>
+				<nav class="nav nav-masthead justify-content-center">
+					<a class="nav-link" href="/coding-session">Coding Career Session</a>
+				</nav>
+			</div>
+		</header>
+		<br>
+	`);
