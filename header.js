f3ed4789b4227b4a775d82c2a5fe1b46b515f911
Ali Khundmiri <salikhundmiri@gmail.com>
2020-10-01 19:47:05 +0530

    updated header

--- header.js
+++ header.js
@@ -3,9 +3,9 @@
 document.write(`
 		<header class="masthead mb-auto">
 			<div class="inner">
-				<h3 class="masthead-brand">Ali Coder Maker</h3>
+				<h3 class="masthead-brand"><a href='https://www.alicodermaker.com/'>Ali Coder Maker</a></h3>
 				<nav class="nav nav-masthead justify-content-center">
-					<a class="nav-link" href="/coding-session">Coding Career Session</a>
+					<a class="nav-link" href="https://calendly.com/alicodermaker/30min">30min 1-on-1</a>
 				</nav>
 			</div>
 		</header>
